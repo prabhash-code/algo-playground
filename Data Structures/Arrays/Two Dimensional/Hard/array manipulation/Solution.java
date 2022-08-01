7ec7efbbd776dcdd89a6845976f9cf73c27e0ccd
pkar3424 <pkar3424@syscolabs.com>
2022-08-01 22:36:44 +0530

    sparse arrays

---
+++ Data Structures/Arrays/Two Dimensional/Hard/array manipulation/Solution.java
@@ -0,0 +1,52 @@
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Solution {
+
+  /*
+   * Complete the 'arrayManipulation' function below.
+   *
+   * The function is expected to return a LONG_INTEGER.
+   * The function accepts following parameters:
+   *  1. INTEGER n
+   *  2. 2D_INTEGER_ARRAY queries
+   */
+
+  public static long arrayManipulation(int n, List<List<Integer>> queries) {
+    // brute force solution
+    Map<Integer, Long> map = new HashMap<>();
+    Long largest = 0L;
+
+    for(int i = 0; i < queries.size(); i++){
+      int start = queries.get(i).get(0);
+      int end = queries.get(i).get(1);
+      int k = queries.get(i).get(2);
+
+      for(int j = start; j <= end; j++){
+        Long val = map.get(j);
+        if(val == null){
+          map.put(j, Long.valueOf(k));
+        } else{
+          map.put(j, val + Long.valueOf(k));
+        }
+
+        if(map.get(j) != null && map.get(j) > largest)
+          largest = map.get(j);
+      }
+
+    }
+
+    return largest;
+  }
+
+}

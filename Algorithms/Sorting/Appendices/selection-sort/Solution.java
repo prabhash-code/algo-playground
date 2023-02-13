9dac6af807e0e62a32ab01dc713367cbc7374fa7
Prabhash Karunathila <prabhash.karunathilaka@syscolabs.com>
2023-02-13 21:32:42 +0530

    Selection Sort

--- Algorithms/Sorting/Appendices/selection-sort/Solution.java
+++ Algorithms/Sorting/Appendices/selection-sort/Solution.java
@@ -11,27 +11,19 @@ import java.util.stream.*;
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 
-class BubbleSort {
+class SelectionSort {
 
-    static void bubbleSort(int[] array) {
-        int size = array.length;
-
-        // loop to access each array element
-        for (int i = 0; i < size - 1; i++) {
-
-            // loop to compare array elements
-            for (int j = 0; j < size - i - 1; j++) {
-
-                // compare two adjacent elements
-                if (array[j] > array[j + 1]) {
-
-                    // swapping occurs if elements
-                    // are not in the intended order
-                    int temp = array[j];
-                    array[j] = array[j + 1];
-                    array[j + 1] = temp;
+    static void selectionSort(int[] array) {
+        for(int i = 0; i < array.length; i++){
+            int smallest = i;
+            for(int j = i+1; j < array.length; j++){
+                if(array[smallest] > array[j]){
+                    smallest = j;
                 }
             }
+            int temp = array[i];
+            array[i] = array[smallest];
+            array[smallest] = temp;
         }
     }
 
